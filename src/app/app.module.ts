d323b502e69e0922b15845e6104bf870a235a8ee
Administrator <tranvanthanh060699@gmail.com>
2022-06-10 19:17:57 +0700

    up code

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {ClassroomComponent} from "./classroom/classroom.component";
+import {StudentComponent} from "./student/student.component";
+import {ProductComponent} from "./product/product.component";
+import {ProductsComponent} from "./products/products.component";
+import {WeatherComponent} from "./weather/weather.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+
+const appRouters: Routes = [
+  {
+    path:'lop-hoc', component: ClassroomComponent
+  },
+  {
+    path:'san-pham', component: ProductsComponent
+  },
+  {
+    path:'thoi-tiet', component: WeatherComponent
+  }
+
+
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClassroomComponent,
+    StudentComponent,
+    ProductComponent,
+    ProductsComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRouters)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
